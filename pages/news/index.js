1c3cf260572c5330058efab8fdb8ed69b8d4431e
Ryan Cabanas <ryangithub777@gmail.com>
2022-09-09 23:17:19 -0400

    End of Lesson #327:
    - Use the `Link` component from the `next/link` library.  This works
      just like an anchor tag.  It even responds to anchor tag CSS styling.
      This allows you to route to other pages without page reloads. Basically
      it's like the `Link` component in the `react-router-dom` library.
    
      If you just used anchor tags `<a href></a>`, this would cause new page
      calls to the server and you would lose state info.
    
      Another interesting thing about the `href` property here is that you
      must declare the entire path.  You can't decalre relative paths.

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,20 @@
 // our-domain.com/news
+import Link from "next/link";
 
 const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/great">Next.js is Great!</Link>
+        </li>
+        <li>
+          <Link href="/news/stinks">Biden stinks!</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
